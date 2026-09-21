34e4654f251ead1c3ecba356a342b7c4b78c0095
Bill Odom <bill@billodom.com>
2026-09-21 11:29:33 -0400

    Reformat code

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -5,8 +5,7 @@ describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-    })
-      .compileComponents();
+    }).compileComponents();
   });
 
   it('should create the app', () => {
@@ -19,6 +18,8 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, workshop-app');
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'Hello, workshop-app',
+    );
   });
 });
