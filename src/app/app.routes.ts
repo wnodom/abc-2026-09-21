a4088fad9d0ae696faf74b166eea51e1b343408a
Bill Odom <bill@billodom.com>
2026-09-22 09:12:04 -0400

    Add first draft of routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { About } from './about/about';
+import { Help } from './help/help';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'about', component: About },
+  { path: 'help', component: Help },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { About } from './about/about';
-import { Help } from './help/help';
+import {
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 
 @Component({
-  imports: [RouterOutlet, About, Help],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
