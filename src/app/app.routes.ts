00b11ca389bfd0a103ac5e89cc113cd5d00f585e
Bill Odom <bill@billodom.com>
2026-09-22 09:18:06 -0400

    Redirect for empty and bad paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { About } from './about/about';
 import { Help } from './help/help';
 
 export const routes: Routes = [
+  { path: '', redirectTo: 'about', pathMatch: 'full' },
   { path: 'about', component: About },
   { path: 'help', component: Help },
+  { path: '**', redirectTo: 'help' },
 ];
