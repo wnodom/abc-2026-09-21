6dd422641d5caf7cd0453d149c38adcf7e88c544
Bill Odom <bill@billodom.com>
2026-09-23 15:39:13 -0400

    Minor cleanup

--- src/app/dashboard/video-list/video-list.ts
+++ src/app/dashboard/video-list/video-list.ts
@@ -1,15 +1,6 @@
-import {
-  Component,
-  inject,
-  input,
-  output,
-  signal,
-} from '@angular/core';
+import { Component, input, output, signal } from '@angular/core';
 import { VideoThumbnail } from '../video-thumbnail/video-thumbnail';
 import { Video } from '../../types';
-
-import { toSignal } from '@angular/core/rxjs-interop';
-import { VideoDataService } from '../../video-data-service';
 
 @Component({
   imports: [VideoThumbnail],
@@ -19,22 +10,15 @@ import { VideoDataService } from '../../video-data-service';
 })
 export class VideoList {
   public readonly videos = input.required<Video[]>();
-
   public readonly selectVideo = output<Video>();
 
   protected readonly showDetails = signal(true);
-
   protected readonly chosenVideo = signal<Video | undefined>(
     undefined,
   );
 
   protected pickVideo(video: Video) {
     this.chosenVideo.set(video);
-
     this.selectVideo.emit(video);
   }
-
-  // private readonly currentVideoLogger = effect(() => {
-  //   console.log(this.currentVideo());
-  // });
 }
