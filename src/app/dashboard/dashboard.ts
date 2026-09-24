d521eeb8e0bbdda1a5bd32cdd027789ee0ad89d3
Bill Odom <bill@billodom.com>
2026-09-24 10:22:53 -0400

    Add stat-filters component

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -9,9 +9,10 @@ import { VideoPlayer } from './video-player/video-player';
 import { Video } from '../types';
 import { VideoDataService } from '../video-data-service';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { StatFilters } from './stat-filters/stat-filters';
 
 @Component({
-  imports: [VideoList, VideoPlayer],
+  imports: [VideoList, VideoPlayer, StatFilters],
   selector: 'ns-dashboard',
   styleUrl: './dashboard.scss',
   templateUrl: './dashboard.html',
